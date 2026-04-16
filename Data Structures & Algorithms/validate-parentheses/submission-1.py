247cad31ebc541a88aef6db9a1dfba87091478fb
CasterlyGit <tarunsp23@gmail.com>
2026-04-15 20:05:22 -0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+        memory = {')' : '(', ']' : '[', '}' : '{'}
+
+        for char in s:
+            if char in memory:
+                if not stack:
+                    return False
+                if stack[-1] == memory[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
